8c1611dc8d663cf262e0bbd0a22ad3de1691e035
TomasAcua <tomas.acuna@est.fi.uncoma.edu.ar>
2024-06-03 21:56:52 -0300

    mejoras

--- script.js
+++ script.js
@@ -11,18 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const personajesEncontrados = document.getElementById("personajes-encontrados");
     const retirarseBtn = document.getElementById("retirarse-btn");
     const listaLogros = document.getElementById("lista-logros");
+    const musicaFondo = document.getElementById("musica-fondo");
 
     let historia = "";
     let lugaresVisitados = new Set();
     let personajesEncontradosSet = new Set();
     let logros = {
-        logro1: false,
-        logro2: false,
-        logro3: false,
-        logro4: false,
-        logro5: false,
-        logro6: false
+        heroe_pueblo: false,
+        lider_bandidos: false,
+        amigo_elfos: false,
+        asesinado_aranas: false,
+        rey_reino: false,
+        devorado_dragon: false,
+        maldicion_cerdo: false
     };
+    let magoEncontrado = false;
+
+    musicaFondo.volume = 0.1;
+    musicaFondo.play();
 
     function mostrarSeccion(id) {
         secciones.forEach(seccion => seccion.classList.remove("active"));
@@ -60,6 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
         historia = "Comienzas tu aventura en un pequeño pueblo...";
         personajePrincipal.src = "imagenes/guerrero.jpg";
         personajePrincipal.alt = "Personaje Principal";
+        magoEncontrado = false;
         textoHistoria.innerHTML = `<p>${historia}</p>`;
         mostrarSeccion("historia");
         actualizarOpcionesIniciales();
@@ -72,6 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
             lugaresVisitados = new Set(estadoGuardado.lugaresVisitados);
             personajesEncontradosSet = new Set(estadoGuardado.personajesEncontrados);
             logros = estadoGuardado.logros;
+            magoEncontrado = estadoGuardado.magoEncontrado;
             textoHistoria.innerHTML = `<p>${historia}</p>`;
             mostrarSeccion("historia");
         } else {
@@ -85,10 +93,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         switch(opcion) {
             case "aldea":
-                historia += "<br>Decides ir a la aldea. Al llegar, encuentras a unos bandidos atacando el pueblo.";
-                mostrarPersonajesSecundarios(["bandido1", "bandido2"]);
+                historia += "<br>Decides ir a la aldea. Al llegar, encuentras a unos bandidos atacando el pueblo y a un aldeano pidiendo ayuda.";
+                mostrarPersonajesSecundarios(["bandido1", "bandido2", "aldeano"]);
                 personajesEncontradosSet.add("bandido1");
                 personajesEncontradosSet.add("bandido2");
+                personajesEncontradosSet.add("aldeano");
                 actualizarOpcionesAldea();
                 break;
             case "bosque":
@@ -108,24 +117,25 @@ document.addEventListener("DOMContentLoaded", () => {
             case "bandidos":
                 historia += "<br>Decides enfrentarte a los bandidos y logras apresarlos, salvando el pueblo.";
                 mostrarPersonajesSecundarios([]);
-                logros.logro1 = true;
+                logros.heroe_pueblo = true;
                 finalizarHistoria("¡Has desbloqueado el logro de Héroe del Pueblo!");
                 break;
             case "ayudar_bandidos":
                 historia += "<br>Decides ayudar a los bandidos a saquear la aldea.";
                 mostrarPersonajesSecundarios([]);
-                logros.logro2 = true;
+                logros.lider_bandidos = true;
                 finalizarHistoria("¡Has desbloqueado el logro de Líder de los Bandidos!");
                 break;
             case "aranas":
                 historia += "<br>Ayudas a los elfos a eliminar las arañas gigantes, ganándote su gratitud.";
                 mostrarPersonajesSecundarios([]);
-                logros.logro3 = true;
+                logros.amigo_elfos = true;
                 finalizarHistoria("¡Has desbloqueado el logro de Amigo de los Elfos!");
                 break;
             case "ayudar_aranas":
                 historia += "<br>Decides no ayudar a los elfos y sigues tu camino. Las arañas te emboscan y te asesinan.";
                 mostrarPersonajesSecundarios([]);
+                logros.asesinado_aranas = true;
                 finalizarHistoria("¡Has sido asesinado por las arañas!");
                 break;
             case "dragon":
@@ -133,38 +143,41 @@ document.addEventListener("DOMContentLoaded", () => {
                 personajePrincipal.src = "imagenes/heroe-rey.jpg";
                 personajePrincipal.alt = "Heroe Rey";
                 mostrarPersonajesSecundarios([]);
-                logros.logro4 = true;
+                logros.rey_reino = true;
                 finalizarHistoria("¡Has desbloqueado el logro de Rey del Reino!");
                 break;
             case "no_dragon":
                 historia += "<br>Decides no enfrentarte al dragón y vuelves al castillo. El dragón te encuentra y te come.";
                 mostrarPersonajesSecundarios([]);
+                logros.devorado_dragon = true;
                 finalizarHistoria("¡Has sido devorado por el dragón!");
                 break;
             case "comprar_pociones":
                 historia += "<br>Compras las pociones del mago y él te regala una espada encantada.";
                 mostrarPersonajesSecundarios([]);
-                logros.logro5 = true;
+                logros.espada_encantada = true;
                 actualizarOpcionesPostMago("espada_encantada");
                 break;
             case "robar_pociones":
                 historia += "<br>Intentas robar las pociones del mago, pero él te maldice y te convierte en un cerdo.";
-                personajePrincipal.src = "imagenes/cerdo.avif";
+                personajePrincipal.src = "imagenes/cerdo.jpg";
                 personajePrincipal.alt = "Cerdo";
                 mostrarPersonajesSecundarios([]);
-                logros.logro6 = true;
+                logros.maldicion_cerdo = true;
                 finalizarHistoria("¡Has sido maldecido y convertido en un cerdo!");
                 break;
         }
 
         textoHistoria.innerHTML = `<p>${historia}</p>`;
 
-        if (Math.random() < 0.2 && opcion !== "comprar_pociones" && opcion !== "robar_pociones") {
+        if (Math.random() < 0.3 && opcion !== "comprar_pociones" && opcion !== "robar_pociones" && !magoEncontrado) {
             historia += "<br>Encuentras a un mago misterioso que te ofrece pociones mágicas.";
+            cambiarFondo("misterioso");
             textoHistoria.innerHTML = `<p>${historia}</p>`;
             mostrarPersonajesSecundarios(["mago"]);
             personajesEncontradosSet.add("mago");
             actualizarOpcionesMago();
+            magoEncontrado = true;
         }
 
         guardarEstado();
@@ -242,24 +255,22 @@ document.addEventListener("DOMContentLoaded", () => {
     function mostrarPersonajesSecundarios(personajes) {
         personajesSecundarios.innerHTML = "";
         personajes.forEach(personaje => {
-            const item = document.createElement("div");
-            item.className = "galeria-item";
             const img = document.createElement("img");
             img.src = `imagenes/${personaje}.jpg`;
             img.alt = personaje;
-            const p = document.createElement("p");
-            p.textContent = personaje;
-            item.appendChild(img);
-            item.appendChild(p);
-            personajesSecundarios.appendChild(item);
+            personajesSecundarios.appendChild(img);
         });
     }
 
     function actualizarGaleriaFondos() {
+        const lugares = ["aldea", "bosque", "castillo", "cueva", "misterioso"];
         imagenesFondo.innerHTML = "";
-        lugaresVisitados.forEach(lugar => {
+        lugares.forEach(lugar => {
             const item = document.createElement("div");
             item.className = "galeria-item";
+            if (!lugaresVisitados.has(lugar)) {
+                item.classList.add("hidden");
+            }
             const img = document.createElement("img");
             img.src = `imagenes/${lugar}.jpg`;
             img.alt = lugar;
@@ -272,10 +283,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function actualizarGaleriaPersonajes() {
+        const personajes = ["bandido1", "bandido2", "elfo", "araña1", "araña2", "rey", "mago", "cerdo", "heroe-rey", "aldeano"];
         personajesEncontrados.innerHTML = "";
-        personajesEncontradosSet.forEach(personaje => {
+        personajes.forEach(personaje => {
             const item = document.createElement("div");
             item.className = "galeria-item";
+            if (!personajesEncontradosSet.has(personaje)) {
+                item.classList.add("hidden");
+            }
             const img = document.createElement("img");
             img.src = `imagenes/${personaje}.jpg`;
             img.alt = personaje;
@@ -289,19 +304,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function actualizarLogros() {
         listaLogros.innerHTML = "";
-        Object.keys(logros).forEach(logro => {
+        for (const [logro, desbloqueado] of Object.entries(logros)) {
             const div = document.createElement("div");
             div.className = "logro";
-            div.textContent = logros[logro] ? logro.replace("logro", "Logro ") : "?";
+            div.textContent = desbloqueado ? logro.replace(/_/g, ' ') : "?";
             listaLogros.appendChild(div);
-        });
+        }
     }
 
     function finalizarHistoria(mensaje) {
-        historia += `<br><br>${mensaje}<br><button id="reiniciar-btn">Iniciar Nueva Aventura</button>`;
+        historia += `<br><br>${mensaje}<br><button id="reiniciar-btn">Ver Logros</button>`;
         textoHistoria.innerHTML = `<p>${historia}</p>`;
         document.getElementById("reiniciar-btn").addEventListener("click", () => {
-            iniciarNuevaAventura();
+            mostrarSeccion("logros");
+            actualizarLogros();
         });
     }
 
@@ -322,7 +338,8 @@ document.addEventListener("DOMContentLoaded", () => {
             historia,
             lugaresVisitados: Array.from(lugaresVisitados),
             personajesEncontrados: Array.from(personajesEncontradosSet),
-            logros
+            logros,
+            magoEncontrado
         };
         guardarJuego(estadoJuego);
     }
@@ -333,6 +350,7 @@ document.addEventListener("DOMContentLoaded", () => {
         lugaresVisitados = new Set(estadoCargado.lugaresVisitados);
         personajesEncontradosSet = new Set(estadoCargado.personajesEncontrados);
         logros = estadoCargado.logros;
+        magoEncontrado = estadoCargado.magoEncontrado;
         textoHistoria.innerHTML = `<p>${historia}</p>`;
         actualizarOpcionesIniciales();
     }
